19f3b15aa4839f27a5dc1e436548024a04839736
unknown <leonyljayme@gmail.com>
2024-02-27 14:58:55 +0800

    add

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import Screen
+from food import Food
+from scoreboard import Scoreboard
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.cv._rootwindow.resizable(False, False)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        food.refresh()
+
+
+screen.mainloop()
